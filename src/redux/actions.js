b27ff5af97584264e2e247a8ae899779f7a227ab
Sona Gharagyozyan <sona.gharagyozyan@gmail.com>
2024-02-07 01:56:44 +0400

    Bugfix (#24)
    * fix: bugs (WIP)
    
    * fix: bugs

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,8 +2,6 @@ export const setPieceInfo = (data) => ({ type: 'SET_PIECE_INFO', data });
 
 export const setDeleteModeFalse = () => ({ type: 'SET_DELETE_MODE_FALSE' });
 
-export const setPopupStatus = (status) => ({ type: 'SET_POPUP_STATUS', status });
-
 export const toggleDeleteMode = () => ({ type: 'TOGGLE_DELETE_MODE' });
 
 export const togglePopup = (status) => ({ type: 'TOGGLE_POPUP', status });
